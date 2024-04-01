7b55cee3402c16186225d434c0998744fe1e3555
John Hightower <john.hightower@uafs.edu>
2024-04-01 18:02:16 -0500

    Update

--- start.php
+++ start.php
@@ -3,16 +3,31 @@
     include_once "./framework/Router.php";
     include_once "./controllers/Home.php";
     include_once "./controllers/About.php";
+    include_once "./controllers/Login.php";
     include_once "./controllers/UserList.php";
     include_once "./controllers/UserAdd.php";
     include_once "./controllers/UserUpdate.php";
     include_once "./controllers/UserDelete.php";
 
-    $router = new Router();
+    class MyRouter extends Router{
+        public function authCheck($action){
+            $controller = $this->controllers[$action];
+            $access = $controller->getAuth();
+            if($access!="PUBLIC"){
+                if(!isset($_SESSION['loggedin'])){
+                    header("Location: start.php?action=login");
+                    exit;
+                }
+            }
+        }
+    }
+
+    $router = new MyRouter();
     $router->showErrors(0);
 
     $router->addController('home',new Home());
     $router->addController('about',new About());
+    $router->addController('login',new Login());
     $router->addController('userList',new UserList());
     $router->addController('userAdd',new UserAdd());
     $router->addController('userUpdate',new UserUpdate());
